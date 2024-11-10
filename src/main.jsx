fdb27a09f9ac34c9135d874965db524ed62d0d94
Egonboz <egonboz@gmail.com>
2024-11-10 22:15:59 +0100

    4rd try

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import "@fontsource/roboto/700.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/galaxyholding/">
+    <BrowserRouter>
       <GalaxyHolding />
     </BrowserRouter>
   </StrictMode>
